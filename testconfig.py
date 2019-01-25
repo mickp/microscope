f951b66d0f7d5fdec14ae03ebbd1155a1bab819d
Mick Phillips <mick.phillips@gmail.com>
2019-01-25 16:51:54 +0000

    Added atmcd camera to testconfig.

--- testconfig.py
+++ testconfig.py
@@ -8,16 +8,18 @@ Import device classes, then define entries in DEVICES as:
 from microscope.devices import device
 # Import device modules/classes here.
 #from microscope.cameras import andorsdk3
+from microscope.cameras import atmcd
 import microscope.testsuite.devices as testdevices
 
 DEVICES = [
     device(testdevices.TestCamera, '127.0.0.1', 8000,),
-	device(testdevices.TestCamera, '127.0.0.1', 8001, com=6, baud=115200),
-	device(testdevices.TestLaser, '127.0.0.1', 8002),
-	device(testdevices.TestLaser, '127.0.0.1', 8003),
-	device(testdevices.TestFilterWheel, '127.0.0.1', 8004),
-	device(testdevices.DummyDSP, '127.0.0.1', 8005),
-	device(testdevices.DummySLM, '127.0.0.1', 8006),
+    device(testdevices.TestCamera, '127.0.0.1', 8001, com=6, baud=115200),
+    device(testdevices.TestLaser, '127.0.0.1', 8002),
+    device(testdevices.TestLaser, '127.0.0.1', 8003),
+    device(testdevices.TestFilterWheel, '127.0.0.1', 8004),
+    device(testdevices.DummyDSP, '127.0.0.1', 8005),
+    device(testdevices.DummySLM, '127.0.0.1', 8006),
     device(testdevices.TestFilterWheel, '127.0.0.1', 8007),
+    device(atmcd.AndorAtmcd, '127.0.0.1', 8009, uid='7197'),
 #	device(andorsdk3.AndorSDK3, '127.0.0.1', 8002, uid='VSC-01344'),
 ]
